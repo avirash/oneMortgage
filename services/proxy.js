78ee71d8e7587a15c4e8d45f2336aaa30a5e706f
abenizri <avibenb@gmail.com>
2019-03-12 14:47:07 +0200

    new proxies

--- services/proxy.js
+++ services/proxy.js
@@ -1,55 +1,160 @@
 const _ = require('lodash')
 const proxies = [
-                  { host: '134.255.243.24',
-                    port: 80,
-                    user:'feat',
-                    pwd:'Aqswdefr!'
-                  },
-                  { host: '109.230.203.32',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '185.121.138.177',
-                    port: 80,
-                    user: 'feat',
-                    pwd: ':Aqswdefr!'
-                  },
-                  { host: '191.101.116.153',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '185.121.138.96',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '134.255.243.189',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '165.231.45.107',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '191.101.116.136',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '109.230.203.82',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  },
-                  { host: '165.231.45.167',
-                    port: 80,
-                    user: 'feat',
-                    pwd: 'Aqswdefr!'
-                  }
+    { host: '109.230.203.55',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.249',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '109.230.220.213',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.27',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.239',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '185.121.138.129',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '109.230.220.159',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.245.160.74',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '89.35.31.98',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '185.193.39.70',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.245.160.99',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.245',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '185.193.39.93',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.89',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '134.255.243.89',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '89.35.31.163',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '109.230.220.21',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '165.231.45.2',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '185.121.138.213',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.196.253.22',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '109.230.203.198',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.196.253.108',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '88.150.182.206',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '185.182.49.10',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '191.101.116.139',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.196.252.99',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '165.231.45.95',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '165.231.45.249',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '196.196.253.246',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '165.231.45.95',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    },
+    { host: '109.230.220.141',
+      port: 80,
+      user:'feat',
+      pwd:'Aqswdefr!'
+    }
 ]
 
 module.exports = class Proxy {
